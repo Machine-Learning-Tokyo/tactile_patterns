5c8f6761530420d956885d487bb851ef7659ad65
John Lau <jolks@outlook.com>
2019-03-06 12:43:30 +0900

    Update comments

--- photo2tactile.py
+++ photo2tactile.py
@@ -1,3 +1,18 @@
+"""
+Convert scenery image to tactile image.
+
+# Algorithm
+1. Read scenery image.
+2. Grayscale.
+3. Histogram equalize.
+4. Compute fine-grained saliency map.
+5. Scale to [0, 255]
+6. Compute binary threshold map.
+7. Invert binary threshold map.
+8. Write tactile image.
+
+References at the corresponding source code below.
+"""
 import argparse
 import cv2
 
@@ -9,16 +24,18 @@ ap.add_argument('-o', '--output', required=False, help='Output: tactile image fi
 args = ap.parse_args()
 
 # Read image, grayscale, equalize
-# https://docs.opencv.org/3.4.3/d4/d1b/tutorial_histogram_equalization.html
+# Source: https://docs.opencv.org/3.4.3/d4/d1b/tutorial_histogram_equalization.html
 image = cv2.imread(args.image)
 image_grayscaled = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 image_equalized = cv2.equalizeHist(image_grayscaled)
 
-# Fine-grained saliency detection from
-# Sebastian Montabone and Alvaro Soto.
-# Human detection using a mobile platform and novel features derived from a visual saliency mechanism.
-# In Image and Vision Computing, Vol. 28 Issue 3, pages 391–402. Elsevier, 2010.
-# https://docs.opencv.org/3.4.3/da/dd0/classcv_1_1saliency_1_1StaticSaliencyFineGrained.html
+"""
+Fine-grained saliency detection from
+Sebastian Montabone and Alvaro Soto.
+Human detection using a mobile platform and novel features derived from a visual saliency mechanism.
+In Image and Vision Computing, Vol. 28 Issue 3, pages 391–402. Elsevier, 2010.
+Source: https://docs.opencv.org/3.4.3/da/dd0/classcv_1_1saliency_1_1StaticSaliencyFineGrained.html
+"""
 fine_saliency = cv2.saliency.StaticSaliencyFineGrained_create()
 _, fine_saliency_map = fine_saliency.computeSaliency(image_equalized)
 
